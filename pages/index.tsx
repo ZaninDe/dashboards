e94326e68013973d43b479fdd9adaf5e3624e897
GabrielZanin1997 <gabriel.zaninde@gmail.com>
2022-06-13 15:31:51 -0300

    button fecthing github user

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,22 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios';
+import { AxiosError } from '../node_modules/axios/index';
+
 export default function Home() {
+  async function getUser() {
+    const user = await axios.get('https://api.github.com/users/ZaninDe')
+      .then(res => res.data)
+    console.log(user)
+  }
+
   return (
-    <h1 className='underline'>hello</h1>
+    <>
+      <button
+        className='p-5 bg-sky-600 hover:bg-sky-700 text-white m-20 rounded'
+        onClick={getUser}
+      >
+        Show User
+      </button>
+    </>
   )
 }
